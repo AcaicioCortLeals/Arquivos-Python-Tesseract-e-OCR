d42e1d5215c7ac5a6c430a5a5a4d68130057ab31
AcacioCortLeals <91157871+AcaicioCortLeals@users.noreply.github.com>
2021-12-07 16:12:08 -0300

    upload dos arquivos
    Arquivos do Tcc usando OCR do Opencv e Tesseract para detectar e reconhecer caractere

---
+++ projeto_Alexandre_Placa/testeocr.py
@@ -0,0 +1,29 @@
+import cv2
+import pytesseract
+
+
+#incorporar imagem
+img = cv2.imread("xerox.png") 
+
+caminho = r'C:\Users\aa\AppData\Local\Programs\Tesseract-OCR'
+
+valor = r'\tesseract.exe'
+
+
+pytesseract.pytesseract.tesseract_cmd = caminho+valor
+
+texto = pytesseract.image_to_string(img)
+
+print(texto)
+cv2.imshow("imagem", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
